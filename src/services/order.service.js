6a1d2592c4c4a6758ae44f36549bd1b9cbf879e5
NTKhang <21520963@gm.uit.edu.vn>
2023-12-13 03:22:12 +0700

    update API update status order with cancelled status

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const { BadRequestError } = require("../core/error-response");
+const { updateInventoryStock } = require("../models/repositories/inventory");
 const { getOrdersByUser, updateOrderById } = require("../models/repositories/order");
 const { convertToObjectId } = require("../utils");
+const ProductService = require("./product.service");
 
 class OrderService {
     static getOrdersByCustomer = async (payload) => {
@@ -25,7 +28,20 @@ class OrderService {
     }
     static updateOrderStatusById = async (payload) => {
         const {orderId, status} = payload;
-        return await updateOrderById(orderId, {order_status: status});
+        const order = await updateOrderById(orderId, {order_status: status});
+        if (!order) throw new BadRequestError("Update order status failed");
+        if (status == 'cancelled') {
+            const {order_products = []} = order;
+            for (let i = 0; i < order_products.length; i++) {
+                const {item_products} = order_products[i];
+                const {productId, product_quantity} = item_products[0];
+                const updateInventory = await updateInventoryStock(productId, parseInt(product_quantity));
+                if (!updateInventory) throw new BadRequestError('Update inventory failed!!!');
+                const updateProduct = await ProductService.updateProductById(productId, {product_quantity: updateInventory.inventory_stock});
+                if (!updateProduct) throw new BadRequestError('Update product failed!!!');
+            }
+        }
+        return order;
     }
 }
 
