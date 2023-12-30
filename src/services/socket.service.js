1da6c0e8e46c5346eb67a53521a99c8408edc303
NTKhang <21520963@gm.uit.edu.vn>
2023-12-31 00:02:59 +0700

    update socket service connection

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -2,18 +2,24 @@
 const mongoose = require('mongoose');
 
 class SocketService {
+  constructor() {
+    this.isListenerAdded = false;
+  }
     connection(socket) {
         socket.on('disconnect', () => {
             console.log('A user disconnected');
         });
 
-        const notificationChangeStream = mongoose.connection.collection('notifications').watch();
-        notificationChangeStream.on('change', (change) => {
-          if (change.operationType === 'insert' || change.operationType === 'update' || change.operationType === 'replace') {
-            console.log(">>>change: ", change.updateDescription);
-            _io.emit('notificationChange', change.updateDescription);
-          }
-        });
+        if (!this.isListenerAdded) {
+          const notificationChangeStream = mongoose.connection.collection('notifications').watch();
+          notificationChangeStream.on('change', (change) => {
+            if (change.operationType === 'insert' || change.operationType === 'update' || change.operationType === 'replace') {
+              console.log(">>>change: ", change.updateDescription);
+              _io.emit('notificationChange', change.updateDescription);
+            }
+          });
+          this.isListenerAdded = true; 
+        }
     }
 }
 
