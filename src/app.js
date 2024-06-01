78c7fcbbe9d40c5547c2cd37a5af7b0a902c4ac7
QuangDuong2903 <quangduongptsc@gmail.com>
2024-06-01 09:44:05 +0700

    add monitoring

--- src/app.js
+++ src/app.js
@@ -30,6 +30,21 @@ app.use(
 // init database
 instanceMongoDb;
 
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+const Registry = client.Registry;
+const register = new Registry();
+collectDefaultMetrics({ register });
+
+app.get('/metrics', async (_req, res) => {
+  try {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  } catch (err) {
+    res.status(500).end(err);
+  }
+});
+
 // init routes
 app.use('/', require('./routes/index'));
 
