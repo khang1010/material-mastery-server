34bd6e0e065d1f8742bdd513da28dd9333ba2a51
khangnt <khangnt@zigvy.com>
2024-11-21 01:17:30 +0700

    feat: update user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,114 +2,127 @@
 
 const mongoose = require('mongoose');
 
-const DOCUMENT_NAME = 'user'
-const COLLECTION_NAME = 'users' 
+const DOCUMENT_NAME = 'user';
+const COLLECTION_NAME = 'users';
 
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
+var userSchema = new mongoose.Schema(
+  {
     avatar: {
-        type: String,
-        default: ''
+      type: String,
+      default: '',
     },
     username: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        default: null,
+      type: String,
+      default: null,
     },
     display_name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     phone: {
-        type: String,
-        default: '',
+      type: String,
+      default: '',
     },
     status: {
-        type: String,
-        enum: ['active', 'inactive'],
-        default: 'inactive'
+      type: String,
+      enum: ['active', 'inactive'],
+      default: 'inactive',
     },
     isBlocked: {
-        type: mongoose.Schema.Types.Boolean,
-        default: false
+      type: mongoose.Schema.Types.Boolean,
+      default: false,
     },
     roles: {
-        type: Array,
-        default: [],
+      type: Array,
+      default: [],
     },
     user_attributes: {
-        type: mongoose.Schema.Types.Mixed,
-        required: true
-    }
-}, {
+      type: mongoose.Schema.Types.Mixed,
+      required: true,
+    },
+  },
+  {
     timestamps: true,
-    collection: COLLECTION_NAME
-});
+    collection: COLLECTION_NAME,
+  }
+);
 
-var customerSchema = new mongoose.Schema({
+var customerSchema = new mongoose.Schema(
+  {
     address: {
-        type: String,
-        required: true,
-        default: ''
+      type: String,
+      required: true,
+      default: '',
     },
     isVip: {
-        type: mongoose.Schema.Types.Boolean,
-        default: false
-    }
-}, {
+      type: mongoose.Schema.Types.Boolean,
+      default: false,
+    },
+    address_info: { type: Object, default: {} },
+  },
+  {
     timestamps: true,
-    collection: 'customers'
-});
+    collection: 'customers',
+  }
+);
 
-var staffSchema = new mongoose.Schema({
+var staffSchema = new mongoose.Schema(
+  {
     positionId: {
-        type: String,
-        required: true,
-        default: ''
+      type: String,
+      required: true,
+      default: '',
     },
     staff_start_date: {
-        type: Date,
-        require: true,
+      type: Date,
+      require: true,
     },
     salary: {
-        type: Number,
-        required: true
-    }
-}, {
+      type: Number,
+      required: true,
+    },
+  },
+  {
     timestamps: true,
-    collection: 'staffs'
-});
+    collection: 'staffs',
+  }
+);
 
-var managerSchema = new mongoose.Schema({
+var managerSchema = new mongoose.Schema(
+  {
     positionId: {
-        type: String,
-        required: true,
-        default: ''
+      type: String,
+      required: true,
+      default: '',
     },
     manager_start_date: {
-        type: Date,
-        require: true,
+      type: Date,
+      require: true,
     },
     salary: {
-        type: Number,
-        required: true
-    }
-}, {
+      type: Number,
+      required: true,
+    },
+  },
+  {
     timestamps: true,
-    collection: 'managers'
-});
+    collection: 'managers',
+  }
+);
 
 //Export the model
 module.exports = {
-    user: mongoose.model(DOCUMENT_NAME, userSchema),
-    customer: mongoose.model('customer', customerSchema),
-    staff: mongoose.model('staff', staffSchema),
-    manager: mongoose.model('manager', managerSchema)
+  user: mongoose.model(DOCUMENT_NAME, userSchema),
+  customer: mongoose.model('customer', customerSchema),
+  staff: mongoose.model('staff', staffSchema),
+  manager: mongoose.model('manager', managerSchema),
 };
