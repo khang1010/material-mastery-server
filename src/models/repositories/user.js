3d66eac2bb8e7dbd89f2fb07a976ba74224fa77b
khangnt <khangnt@zigvy.com>
2024-12-11 01:46:56 +0700

    feat: update password

--- src/models/repositories/user.js
+++ src/models/repositories/user.js
@@ -1,33 +1,62 @@
-'use strict'
+'use strict';
+const bcrypt = require('bcrypt');
 
-const { getSortAscending, getUnSelectData, getSortDescending } = require("../../utils");
-const { user } = require("../user.model");
+const {
+  getSortAscending,
+  getUnSelectData,
+  getSortDescending,
+} = require('../../utils');
+const { user } = require('../user.model');
 
 const findByEmailOrUsername = async (userInfo) => {
-    return await user.findOne({ $or: [{ email: userInfo }, { username: userInfo }] }).lean();
-}
+  return await user
+    .findOne({ $or: [{ email: userInfo }, { username: userInfo }] })
+    .lean();
+};
 
 const findUserById = async (user_id) => {
-    return await user.findById(user_id).lean();
-}
+  return await user.findById(user_id).lean();
+};
 
 const updateUserById = async (user_id, model, payload) => {
-    return await model.findByIdAndUpdate(user_id, payload, {new: true})
-}
+  const { password } = payload;
+  let passwordHash = undefined;
+  if (password) {
+    //Hash password
+    passwordHash = await bcrypt.hash(password, 10);
+  }
+  return await model.findByIdAndUpdate(
+    user_id,
+    { ...payload, password: passwordHash },
+    { new: true }
+  );
+};
 
-const getAllUsers = async ({limit = 50, page = 1, sorted = ["_id"], filter = {}, unSelect = [], isAscending = true}) => {
-    // isAscending === 'true' ? console.log(">>>sort: ", getSortAscending(sorted)) : console.log(">>>sort: ", getSortDescending(sorted));
-    return await user.find(filter)
+const getAllUsers = async ({
+  limit = 50,
+  page = 1,
+  sorted = ['_id'],
+  filter = {},
+  unSelect = [],
+  isAscending = true,
+}) => {
+  // isAscending === 'true' ? console.log(">>>sort: ", getSortAscending(sorted)) : console.log(">>>sort: ", getSortDescending(sorted));
+  return await user
+    .find(filter)
     .skip((page - 1) * limit)
     .limit(limit)
-    .sort(isAscending === 'true' ? getSortAscending(sorted) : getSortDescending(sorted))
+    .sort(
+      isAscending === 'true'
+        ? getSortAscending(sorted)
+        : getSortDescending(sorted)
+    )
     .select(getUnSelectData(unSelect))
-    .lean()
-}
+    .lean();
+};
 
 module.exports = {
-    findByEmailOrUsername,
-    updateUserById,
-    findUserById,
-    getAllUsers
-}
+  findByEmailOrUsername,
+  updateUserById,
+  findUserById,
+  getAllUsers,
+};
